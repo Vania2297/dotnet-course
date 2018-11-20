963b6eeeb474b86dc3b098e40967cc691cd148f8
Shilo Ivan <shiloivan1997@gmail.com>
2018-11-20 14:49:04 +0300

    Added lesson 1

---
+++ Lessons/Lesson1/Operators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Operators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Car { FuelCar = 23 };
+            var c2 = new Car { FuelCar = 45 };
+            var result = c1 > c2;
+            Console.WriteLine(result); // false
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Car
+    {
+        public int Engine { get; set; }
+        public int FuelCar { get; set; }
+
+        public static Car operator +(Car car1, Car car2)
+        {
+            return new Car { FuelCar = car1.FuelCar + car2.FuelCar };
+        }
+        public static bool operator >(Car c1, Car c2)
+        {
+            return c1.FuelCar > c2.FuelCar;
+        }
+        public static bool operator <(Car c1, Car c2)
+        {
+            return c1.FuelCar < c2.FuelCar;
+        }
+
+        public static bool operator true(Car c1)
+        {
+            return c1.FuelCar != 0;
+        }
+        public static bool operator false(Car c1)
+        {
+            return c1.FuelCar == 0;
+        }
+    }
+}
---
+++ Lessons/Lesson1/Square/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Square
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new Shape[3, 11];
+            for (var i = 0; i < 10; i++)
+            {
+                matrix[0, i] = new Circle(i);
+                matrix[1, i] = new Square(i);
+                matrix[2, i] = new Triangle(i);
+            }
+
+            for (var i = 0; i < matrix.Rank+1; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    Console.WriteLine($" {matrix[i, j].GetSquare(),3}");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Shape
+    {
+        public virtual double GetSquare()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private readonly int _radius;
+
+        public Circle(int radius)
+        {
+            this._radius = radius;
+        }
+
+        public override double GetSquare()
+        {
+            return Math.Round(Math.PI * Math.Pow(_radius,2));
+        }
+    }
+
+    public class Square : Shape
+    {
+        private readonly int _length;
+
+        public Square(int lenght)
+        {
+            this._length = lenght;
+        }
+
+        public override double GetSquare()
+        {
+            return (_length * _length);
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        private readonly double _side;
+
+        public Triangle(double side)
+        {
+            this._side = side;
+        }
+
+        public override double GetSquare()
+        { 
+            return Math.Pow(_side, 2) * Math.Sqrt(3) / 4;
+        }
+    }
+}
